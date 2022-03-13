5ee87549b87a163d4de1564a5b16e1bdbcf39623
arifulislam12341 <arifulislmmarfy@gmail.com>
2022-03-13 13:21:52 +0600

    task add

---
+++ APWT_TASK_2/app/Http/Controllers/StudentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function registration()
+    {
+        return view('pages.student.registration');
+    }
+    public function registrationSubmit(Request $request)
+    {
+        $validate=$request->validate(
+        [
+            'name'=>'required',
+            'address'=>'address',
+            'email'=>'required',
+            'phonenu'=>'required',
+            'password'=>'required',
+    
+        ],
+        [
+            'name.required'=>'Please Enter Your Name',
+           
+            'phonenu.required'=>'Please Enter Your Phone Number',
+            'email.required'=>'Please Enter Your Email',
+            'password.required'=>'Please Enter Your password',
+        ]
+        );
+    
+    
+    }
+    public function login()
+{
+    return view('pages.student.login');
+}
+public function loginSubmit(Request $request)
+{
+    $validate=$request->validate(
+    [
+        
+       
+        'email'=>'required',
+        'password'=>'required',
+
+    ],
+    [
+        
+     
+        
+        'email.required'=>'Please enter your email',
+        'password.required'=>'Please enter your password',
+    ]
+    );
+
+return "Welcome Marfy ";
+}
+
+  
+}
